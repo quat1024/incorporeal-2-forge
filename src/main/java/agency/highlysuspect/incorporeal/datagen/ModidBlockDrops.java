bf58988a7ca2f82abaf53fd6e8dfc08717e4af3d
quat1024 <quat1024@users.noreply.github.com>
2021-08-09 01:31:15 -0400

    the jokerfication continues

--- src/main/java/agency/highlysuspect/incorporeal/datagen/ModidBlockDrops.java
+++ src/main/java/agency/highlysuspect/incorporeal/datagen/ModidBlockDrops.java
@@ -1,11 +1,15 @@
 package agency.highlysuspect.incorporeal.datagen;
 
+import agency.highlysuspect.rhododendrite.Rho;
+import agency.highlysuspect.rhododendrite.block.RhoBlocks;
+import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import net.minecraft.block.Block;
+import net.minecraft.block.*;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DirectoryCache;
 import net.minecraft.data.IDataProvider;
+import net.minecraft.data.loot.BlockLootTables;
 import net.minecraft.loot.*;
 import net.minecraft.loot.conditions.SurvivesExplosion;
 import net.minecraft.util.ResourceLocation;
@@ -14,7 +18,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.io.IOException;
 import java.nio.file.Path;
 
-public class ModidBlockDrops implements IDataProvider {
+public class ModidBlockDrops extends BlockLootTables implements IDataProvider  {
 	public ModidBlockDrops(String modid, DataGenerator d) {
 		this.modid = modid;
 		this.d = d;
@@ -35,8 +39,19 @@ public class ModidBlockDrops implements IDataProvider {
 			ResourceLocation id = b.getRegistryName(); assert id != null; //FUCKING FORGE
 			if(!id.getNamespace().equals(modid)) continue;
 			
-			//all blocks drop themselves
-			save(cache, b, self(b));
+			if(b instanceof WallSignBlock) continue; //by convention these all "dropsLike" the regular block, no need for an extra table
+			
+			if(b instanceof DoorBlock) {
+				save(cache, b, door(b));
+			} else if(b instanceof SlabBlock) {
+				save(cache, b, slab(b));
+			} else if(b instanceof LeavesBlock) {
+				Preconditions.checkArgument(b.getRegistryName().equals(Rho.id("rhododendrite_leaves")), "fix ur shit");
+				save(cache, b, leaves(b, RhoBlocks.RHODODENDRITE.planks)); //TODO actually drop the sapling when i add that.
+			} else {
+				//all other blocks drop themselves
+				save(cache, b, self(b));
+			}
 		}
 	}
 	
@@ -51,5 +66,17 @@ public class ModidBlockDrops implements IDataProvider {
 		return LootTable.builder().addLootPool(
 			LootPool.builder().name("self").rolls(ConstantRange.of(1)).addEntry(ItemLootEntry.builder(b)).acceptCondition(SurvivesExplosion.builder())
 		).setParameterSet(LootParameterSets.BLOCK).build();
+	}
+	
+	private static LootTable door(Block b) {
+		return BlockLootTables.registerDoor(b).setParameterSet(LootParameterSets.BLOCK).build();
+	}
+	
+	private static LootTable slab(Block b) {
+		return droppingSlab(b).setParameterSet(LootParameterSets.BLOCK).build();
+	}
+	
+	private static LootTable leaves(Block b, Block sapling) {
+		return droppingWithChancesAndSticks(b, sapling, 0.05F, 0.0625F, 0.083333336F, 0.1F).setParameterSet(LootParameterSets.BLOCK).build();
 	}
 }
--- src/main/java/agency/highlysuspect/rhododendrite/datagen/RhoStatesModels.java
+++ src/main/java/agency/highlysuspect/rhododendrite/datagen/RhoStatesModels.java
@@ -1,11 +1,15 @@
 package agency.highlysuspect.rhododendrite.datagen;
 
+import agency.highlysuspect.incorporeal.Inc;
 import agency.highlysuspect.rhododendrite.Rho;
 import agency.highlysuspect.rhododendrite.WoodBlockFamily;
 import agency.highlysuspect.rhododendrite.block.RhoBlocks;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.IForgeRegistryEntry;
 
 import javax.annotation.Nonnull;
 
@@ -20,29 +24,74 @@ public class RhoStatesModels extends BlockStateProvider {
 		return "rhododendrite blockstates and item models";
 	}
 	
+	@SuppressWarnings("ConstantConditions") //shit has registry names
+	private String n(IForgeRegistryEntry<?> b) {
+		return b.getRegistryName().getPath();
+	}
+	
 	@Override
 	protected void registerStatesAndModels() {
 		handleWoodBlockFamily(RhoBlocks.RHODODENDRITE);
 	}
 	@SuppressWarnings("SameParameterValue")
 	private void handleWoodBlockFamily(WoodBlockFamily family) {
+		ResourceLocation planks = family.planks.getRegistryName();
+		
 		simpleBlock(family.planks);
+		blockItemParent(family.planks.asItem());
+		
 		//todo sapling
+		
 		logBlock(family.log);
-		logBlock(family.strippedLog);
+		blockItemParent(family.log.asItem());
+		logBlock(family.strippedLog); 
+		blockItemParent(family.strippedLog.asItem());
 		logBlock(family.wood);
+		blockItemParent(family.wood.asItem());
 		logBlock(family.strippedWood);
+		blockItemParent(family.strippedWood.asItem());
+		
 		//todo leaves
-		stairsBlock(family.stairs, family.planks.getRegistryName());
+		blockItemParent(family.leaves.asItem());
+		
+		stairsBlock(family.stairs, planks);
+		itemModels().stairs(n(family.stairs), planks, planks, planks);
+		
 		//todo sign
+		
 		doorBlock(family.door, Rho.id(family.name + "_door_bottom"), Rho.id(family.name + "_door_top"));
+		itemGenerated(family.door.asItem());
+		
 		//todo wall sign
+		
 		//todo pressure plate
-		fenceBlock(family.fence, family.planks.getRegistryName());
+		
+		fenceBlock(family.fence, planks);
+		itemModels().fenceInventory(n(family.fence), planks);
+		
 		trapdoorBlock(family.trapdoor, Rho.id(family.name + "_trapdoor"), true);
-		fenceGateBlock(family.fenceGate, family.planks.getRegistryName());
+		blockItemParent(family.trapdoor.asItem());
+		
+		fenceGateBlock(family.fenceGate, planks);
+		itemModels().fenceGate(n(family.fenceGate), planks);
+		
 		//todo potted sapling
+		
 		//todo button
-		slabBlock(family.slab, family.planks.getRegistryName(), family.planks.getRegistryName());
+		
+		slabBlock(family.slab, planks, planks);
+		itemModels().slab(n(family.slab), planks, planks, planks);
+	}
+	
+	private void blockItemParent(Item i) {
+		itemModels().withExistingParent(n(i), Rho.id("block/" + n(i)));
+	}
+	
+	private void itemGenerated(IForgeRegistryEntry<?> thingie) {
+		itemGenerated(thingie, thingie.getRegistryName());
+	}
+	
+	private void itemGenerated(IForgeRegistryEntry<?> thingie, ResourceLocation texture) {
+		itemModels().withExistingParent(n(thingie), new ResourceLocation("item/generated")).texture("layer0", texture);
 	}
 }
