d04cee59b726a70ac2c7b589a995a8b2298996dd
quat1024 <quat1024@users.noreply.github.com>
2021-08-10 03:48:26 -0400

    Just bind with wand of the forest lmao

--- src/main/java/agency/highlysuspect/rhododendrite/block/OpcodeBlock.java
+++ src/main/java/agency/highlysuspect/rhododendrite/block/OpcodeBlock.java
@@ -3,18 +3,23 @@ package agency.highlysuspect.rhododendrite.block;
 import agency.highlysuspect.rhododendrite.block.tile.CoreTile;
 import agency.highlysuspect.rhododendrite.block.tile.OpcodeTile;
 import agency.highlysuspect.rhododendrite.block.tile.RhoTileTypes;
+import agency.highlysuspect.rhododendrite.block.tile.RhodoNetworkTile;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+import vazkii.botania.api.wand.IWandable;
 
 import javax.annotation.Nullable;
 
-public class OpcodeBlock extends Block {
+public class OpcodeBlock extends Block implements IWandable {
 	public OpcodeBlock(Properties properties, CoreAction action) {
 		super(properties);
 		this.action = action;
@@ -48,6 +53,16 @@ public class OpcodeBlock extends Block {
 				}
 			}
 		}
+	}
+	
+	//TODO copy to other rhodo network tiles/mess about so the hierarchy is more clear
+	@Override
+	public boolean onUsedByWand(PlayerEntity player, ItemStack stack, World world, BlockPos pos, Direction side) {
+		TileEntity tile = world.getTileEntity(pos);
+		if(tile instanceof RhodoNetworkTile) {
+			((RhodoNetworkTile) tile).sparkle();
+			return true;
+		} else return false;
 	}
 	
 	@Override
--- src/main/java/agency/highlysuspect/rhododendrite/block/tile/RhodoNetworkTile.java
+++ src/main/java/agency/highlysuspect/rhododendrite/block/tile/RhodoNetworkTile.java
@@ -1,132 +1,73 @@
 package agency.highlysuspect.rhododendrite.block.tile;
 
+import agency.highlysuspect.incorporeal.IncNetwork;
 import agency.highlysuspect.rhododendrite.computer.CorePathTracing;
-import net.minecraft.client.Minecraft;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.nbt.NBTUtil;
-import net.minecraft.particles.ParticleTypes;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.registry.Registry;
+import vazkii.botania.api.internal.VanillaPacketDispatcher;
+import vazkii.botania.api.wand.IWandBindable;
 import vazkii.botania.common.block.tile.TileMod;
+import vazkii.botania.common.core.handler.ModSounds;
+import vazkii.botania.common.entity.EntitySpark;
+import vazkii.botania.common.item.ItemTwigWand;
+import vazkii.botania.common.network.PacketHandler;
 
 import javax.annotation.Nullable;
 
-public class RhodoNetworkTile extends TileMod {
+public class RhodoNetworkTile extends TileMod implements IWandBindable {
 	public RhodoNetworkTile(TileEntityType<?> type) {
 		super(type);
 	}
 	
-	public static class Connection {
-		public Connection(BlockPos corePos, BlockPos uplink) {
-			this.corePos = corePos;
-			this.uplink = uplink;
-		}
-		
-		//The location of my network's core.
-		public BlockPos corePos;
-		//The reason I'm able to connect to that core, even if the core itself is out of range.
-		public BlockPos uplink;
-	}
-	
-	private @Nullable Connection connection;
-	private long lastUpkeep = 0; //simple check to avoid two tiles validateConnection-ing each other in a loop
+	public BlockPos uplink;
 	
 	public @Nullable CoreTile findCore() {
-		if(world == null) return null;
+		if(world == null || uplink == null) return null;
 		
-		validateConnection();
-		if(connection == null) return null;
-		
-		else return RhoTileTypes.CORE.getIfExists(world, connection.corePos);
-	}
-	
-	private void validateConnection() {
-		if(world == null) return;
-		if(!upkeepConnection()) rescan();
-	}
-	
-	private boolean upkeepConnection() {
-		if(world == null || connection == null) return false;
-		
-		//This is a bad hack and i should feel bad. Mom we have graph theory at home
-		if(lastUpkeep == world.getGameTime()) return connection != null;
-		lastUpkeep = world.getGameTime();
-		
-		//If my uplink is in an unloaded chunk: just assume the connection is valid.
-		if(!world.getChunkProvider().isChunkLoaded(new ChunkPos(connection.uplink))) return true;
-		TileEntity tile = world.getTileEntity(connection.uplink);
-		
-		//If my uplink is a core, the connection is valid.
-		if(tile instanceof CoreTile) {
-			connection.corePos = connection.uplink; //Just in case??? i should assert for this
-			return true;
+		BlockPos cursor = uplink;
+		while(cursor != null) {
+			TileEntity tile = world.getTileEntity(cursor);
+			if(tile instanceof CoreTile) return (CoreTile) tile;
+			else if(tile instanceof RhodoNetworkTile) cursor = ((RhodoNetworkTile) tile).uplink;
+			else return null;
 		}
 		
-		//If my uplink is another RhodoNetworkTile, my connection is only as valid as its is.
-		if(tile instanceof RhodoNetworkTile && tile != this) {
-			RhodoNetworkTile uplinkTile = (RhodoNetworkTile) tile;
-			uplinkTile.validateConnection();
-			
-			if(uplinkTile.connection == null) {
-				this.connection = null;
-				return false;
-			} else {
-				connection.corePos = uplinkTile.connection.corePos;
-				return true;
-			}
-		}
-		
-		//If I found something else, my connection is not valid.
-		return false;
+		throw new IllegalStateException("unreachable");
 	}
 	
-	private void rescan() {
-		connection = null;
-		if(world == null) return;
+	public void sparkle() {
+		if(world == null || uplink == null) return;
 		
-		//Scan in a sphere around me
-		BlockPos.Mutable pos = this.pos.toMutable();
-		for(BlockPos offset : CorePathTracing.RELATIVE_SCAN_OFFSETS) {
-			pos.func_243531_h(offset); //"add" with a fucked mcp name
-			
-			TileEntity tile = world.getTileEntity(pos);
-			
-			//TODO yes this is obvious testing code
-			Minecraft.getInstance().world.addParticle(ParticleTypes.END_ROD, pos.getX() + .5, pos.getY() + 5, pos.getZ() + .5, 0, 0, 0);
-			
-			//If I find a core directly, that's great, connect to it.
-			if(tile instanceof CoreTile) {
-				BlockPos yes = pos.toImmutable();
-				connection = new Connection(yes, yes);
-				Minecraft.getInstance().world.addParticle(ParticleTypes.LARGE_SMOKE, pos.getX() + .5, pos.getY() + 5, pos.getZ() + .5, 0, 0, 0);
-				return;
-			}
-			
-			//If I find something else with a valid core uplink, connect to it.
-			if(tile instanceof RhodoNetworkTile && tile != this) {
-				RhodoNetworkTile other = (RhodoNetworkTile) tile;
-				other.validateConnection();
-				if(other.connection != null) {
-					Minecraft.getInstance().world.addParticle(ParticleTypes.ANGRY_VILLAGER, pos.getX() + .5, pos.getY() + 5, pos.getZ() + .5, 0, 0, 0);
-					Minecraft.getInstance().world.addParticle(ParticleTypes.NOTE, other.connection.corePos.getX() + .5, other.connection.corePos.getY() + 5, other.connection.corePos.getZ() + .5, 0, 0, 0);
-					connection = new Connection(other.connection.corePos, pos.toImmutable());
-					return;
-				}
-			}
+		BlockPos start = pos;
+		BlockPos end = uplink;
+		while(true) {
+			TileEntity tile = world.getTileEntity(end);
+			if(tile instanceof CoreTile || tile instanceof RhodoNetworkTile)
+				//TODO make a brighter sparkle-line packet. This is very small
+				IncNetwork.sendToNearby(world, pos, new IncNetwork.SparkleLine(Vector3d.copyCentered(start), Vector3d.copyCentered(end), 30));
+				//ItemTwigWand.doParticleBeamWithOffset(world, start, end);
+			if(tile instanceof RhodoNetworkTile) {
+				start = end;
+				end = ((RhodoNetworkTile) tile).uplink;
+				if(end == null) break;
+			} else break;
 		}
-		
-		//If I didn't find anything, the connection was set to `null` up top, which is correct
 	}
 	
 	@Override
 	public void writePacketNBT(CompoundNBT nbt) {
 		super.writePacketNBT(nbt);
 		
-		if(connection != null) {
-			nbt.put("CorePos", NBTUtil.writeBlockPos(connection.corePos));
-			nbt.put("Uplink", NBTUtil.writeBlockPos(connection.uplink));
+		if(uplink != null) {
+			nbt.put("Uplink", NBTUtil.writeBlockPos(uplink));
 		}
 	}
 	
@@ -134,8 +75,39 @@ public class RhodoNetworkTile extends TileMod {
 	public void readPacketNBT(CompoundNBT nbt) {
 		super.readPacketNBT(nbt);
 		
-		if(nbt.contains("CorePos")) {
-			connection = new Connection(NBTUtil.readBlockPos(nbt.getCompound("CorePos")), NBTUtil.readBlockPos(nbt.getCompound("Uplink")));
-		} else connection = null;
+		if(nbt.contains("Uplink")) {
+			uplink = NBTUtil.readBlockPos(nbt.getCompound("Uplink"));
+		} else uplink = null;
+	}
+	
+	@Override
+	public boolean canSelect(PlayerEntity player, ItemStack wand, BlockPos pos, Direction side) {
+		return true;
+	}
+	
+	@Override
+	public boolean bindTo(PlayerEntity player, ItemStack wand, BlockPos pos, Direction side) {
+		if(player == null || this.pos.equals(pos)) return false;
+		
+		if(!CorePathTracing.withinWirelessRange(this.pos, pos)) return false;
+		
+		TileEntity tileAt = player.world.getTileEntity(pos);
+		if(tileAt != this && (tileAt instanceof CoreTile || tileAt instanceof RhodoNetworkTile)){
+			uplink = pos;
+			
+			player.playSound(ModSounds.ding, 0.1F, 1F);
+			
+			markDirty();
+			VanillaPacketDispatcher.dispatchTEToNearbyPlayers(this);
+			sparkle();
+			
+			return true;
+		} else return false;
+	}
+	
+	@Nullable
+	@Override
+	public BlockPos getBinding() {
+		return uplink;
 	}
 }
