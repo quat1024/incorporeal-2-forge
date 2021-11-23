5fd6f7eeeb0ccdb5e5a5bc8688954950d3733d43
quat1024 <quat1024@users.noreply.github.com>
2021-11-23 03:55:58 -0500

    corp solidifier

--- src/main/java/agency/highlysuspect/incorporeal/block/CorporeaSolidifierBlock.java
+++ src/main/java/agency/highlysuspect/incorporeal/block/CorporeaSolidifierBlock.java
@@ -1,52 +1,60 @@
 package agency.highlysuspect.incorporeal.block;
 
 import agency.highlysuspect.incorporeal.item.IncItems;
+import net.minecraft.world.Container;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.HopperBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.core.Direction;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
-import net.minecraftforge.items.IItemHandler;
-import net.minecraftforge.items.ItemHandlerHelper;
 import vazkii.botania.api.corporea.ICorporeaRequestMatcher;
+import vazkii.botania.common.block.tile.corporea.TileCorporeaFunnel;
 import vazkii.botania.common.core.helper.InventoryHelper;
-
-import net.minecraft.world.level.block.state.BlockBehaviour.Properties;
 
 public class CorporeaSolidifierBlock extends Block {
 	public CorporeaSolidifierBlock(Properties properties) {
 		super(properties);
 	}
 	
-	public void receiveRequest(Level world, BlockPos pos, BlockState state, ICorporeaRequestMatcher request, int count) {
-		if(world == null || world.isClientSide) return;
+	public void receiveRequest(Level level, BlockPos pos, BlockState state, ICorporeaRequestMatcher request, int count) {
+		if(level == null || level.isClientSide) return;
 		
 		ItemStack ticket = IncItems.CORPOREA_TICKET.produceForRequest(request, count);
-		IItemHandler dest = getInv(world, pos);
+		Container inv = getInv(level, pos);
 		
-		if(dest != null && ItemHandlerHelper.insertItemStacked(dest, ticket, false).isEmpty()) {
-			ItemHandlerHelper.insertItemStacked(dest, ticket, true);
+		//Copy from TileCorporeaFunnel (modified)
+		if (inv != null && InventoryHelper.simulateTransfer(inv, ticket, Direction.UP).isEmpty()) {
+			HopperBlockEntity.addItem(null, inv, ticket, Direction.UP);
 		} else {
-			world.addFreshEntity(new ItemEntity(world,
-				pos.getX() + .5, pos.getY() + 1, pos.getZ() + .5,
-				ticket
-			));
+			ItemEntity item = new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 1.5, pos.getZ() + 0.5, ticket);
+			level.addFreshEntity(item);
 		}
 	}
 	
-	//Modified from TileCorporeaFunnel.
-	private IItemHandler getInv(Level world, BlockPos pos) {
-		//Try 1 block below
-		IItemHandler ret = InventoryHelper.getInventory(world, pos.below(), Direction.UP);
-		if (ret == null) ret = InventoryHelper.getInventory(world, pos.below(), null);
-		if (ret != null) return ret;
+	//Copy of TileCorporeaFunnel
+	private Container getInv(Level level, BlockPos worldPosition) {
+		BlockEntity te = level.getBlockEntity(worldPosition.below());
+		Container ret = InventoryHelper.getInventory(level, worldPosition.below(), Direction.UP);
+		if (ret == null) {
+			ret = InventoryHelper.getInventory(level, worldPosition.below(), null);
+		}
+		if (ret != null && !(te instanceof TileCorporeaFunnel)) {
+			return ret;
+		}
 		
-		//Try 2 blocks below
-		ret = InventoryHelper.getInventory(world, pos.below(2), Direction.UP);
-		if (ret == null) ret = InventoryHelper.getInventory(world, pos.below(2), null);
+		te = level.getBlockEntity(worldPosition.below(2));
+		ret = InventoryHelper.getInventory(level, worldPosition.below(2), Direction.UP);
+		if (ret == null) {
+			ret = InventoryHelper.getInventory(level, worldPosition.below(2), null);
+		}
+		if (ret != null && !(te instanceof TileCorporeaFunnel)) {
+			return ret;
+		}
 		
-		return ret;
+		return null;
 	}
 }
